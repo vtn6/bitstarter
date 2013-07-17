9f297d4e0456148108a57ed02837101c7c784b99
Vincent Nguyen <vtn6@cornell.edu>
2013-07-17 23:36:42 +0000

    I dont know anymore

--- web.js
+++ web.js
@@ -2,11 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+buf = fs.readFileSync('index.html')
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+console.log("yoo");
