54d60ec3a7ab7aae35dedec04c9c09d5626992b1
Vincent Nguyen <vtn6@cornell.edu>
2013-07-17 23:39:19 +0000

    I dont know anymore2

--- web.js
+++ web.js
@@ -2,10 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var t = "HAI"
+
 buf = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send(buf.toString());
+  response.send(t);
 });
 
 var port = process.env.PORT || 5000;
